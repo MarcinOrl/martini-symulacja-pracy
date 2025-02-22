12b89ce0aef9291fa577b0d907849daa97341d8d
Martini <marcino064@o2.pl>
2025-02-22 20:24:23 +0100

    Implemented sprint mechanic

--- main.gd
+++ main.gd
@@ -2,6 +2,13 @@ extends Node2D
 
 #onready var player = $Player
 onready var nav = $Navigation2D
+onready var sprint_bar = $CanvasLayer/SprintBar
+
+func _process(delta):
+	var player = get_node_or_null("Player")
+	if player:
+		sprint_bar.visible = player.stamina < 5.0
+		sprint_bar.value = (player.stamina / 5.0) * 100
 
 func _on_Timer_timeout():
 	var player = get_node_or_null( "Player" )
